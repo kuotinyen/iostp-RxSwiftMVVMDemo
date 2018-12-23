33f89193a710aedfd776e3da6c41b3daa41877a3
kuotinyen <kuotinyen@gmail.com>
2018-12-23 15:21:29 +0800

    Import Neo project

---
+++ RxSwiftMVVMDemo/Module/PhotoList/PhotoListViewController.swift
@@ -0,0 +1,139 @@
+//
+//  EventListViewController.swift
+//  MVVMPlayground
+//
+//  Created by Neo on 01/10/2017.
+//  Copyright © 2017 ST.Huang. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class PhotoListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    lazy var viewModel: PhotoListViewModel = {
+        return PhotoListViewModel(apiService: APIService())
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Init the static view
+        initView()
+        
+        // init view model
+        initVM()
+        
+    }
+    
+    func initView() {
+        self.navigationItem.title = "Popular"
+        
+        tableView.estimatedRowHeight = 150
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    func initVM() {
+        
+        // Naive binding
+        viewModel.showAlertClosure = { [weak self] message in
+            DispatchQueue.main.async {
+                self?.showAlert( message )
+            }
+        }
+        
+        viewModel.updateLoadingStatus = { [weak self] isLoading in
+            DispatchQueue.main.async {
+                if isLoading {
+                    self?.activityIndicator.startAnimating()
+                    self?.tableView.alpha = 0.0
+                }else {
+                    self?.activityIndicator.stopAnimating()
+                    self?.tableView.alpha = 1.0
+                }
+            }
+        }
+        
+        viewModel.reloadTableViewClosure = { [weak self] () in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        
+        viewModel.viewIsReady()
+
+    }
+    
+    func showAlert( _ message: String ) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction( UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+extension PhotoListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "photoCellIdentifier", for: indexPath) as? PhotoListTableViewCell else {
+            fatalError("Cell not exists in storyboard")
+        }
+        
+        let cellVM = viewModel.getCellViewModel( at: indexPath )
+        
+        cell.nameLabel.text = cellVM.titleText
+        cell.descriptionLabel.text = cellVM.descText
+        cell.mainImageView?.sd_setImage(with: URL( string: cellVM.imageUrl ), completed: nil)
+        cell.dateLabel.text = cellVM.dateText
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfCells
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.0
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        
+        self.viewModel.userPressed(at: indexPath)
+        if viewModel.isAllowSegue {
+            return indexPath
+        }else {
+            return nil
+        }
+    }
+
+}
+
+extension PhotoListViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? PhotoDetailViewController,
+            let photo = viewModel.selectedPhoto {
+            vc.imageUrl = photo.image_url
+        }
+    }
+}
+
+class PhotoListTableViewCell: UITableViewCell {
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descContainerHeightConstraint: NSLayoutConstraint!
+}
+
---
+++ RxSwiftMVVMDemo/Service/APIService.swift
@@ -0,0 +1,38 @@
+//
+//  APIService.swift
+//  MVVMPlayground
+//
+//  Created by Neo on 01/10/2017.
+//  Copyright © 2017 ST.Huang. All rights reserved.
+//
+
+import Foundation
+
+protocol APIServiceProtocol {
+    func fetchPopularPhoto( complete: @escaping ( _ success: Bool, _ photos: [Photo], _ error: Error? )->() )
+}
+
+class APIService: APIServiceProtocol {
+    // Simulate a long waiting for fetching 
+    func fetchPopularPhoto( complete: @escaping ( _ success: Bool, _ photos: [Photo], _ error: Error? )->() ) {
+        DispatchQueue.global().async {
+            sleep(3)
+            let path = Bundle.main.path(forResource: "content", ofType: "json")!
+            let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            let photos = try! decoder.decode(Photos.self, from: data)
+            complete( true, photos.photos, nil )
+        }
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
